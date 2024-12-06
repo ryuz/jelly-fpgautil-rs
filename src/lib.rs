739555f7f1f06a51a239d2c539d054fb84887281
Ryuji Fuchikami <ryuji.fuchikami@nifty.com>
2024-12-06 22:07:46 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -24,21 +24,16 @@ pub fn load(accel_name: &str) -> Result<(), Box<dyn Error>> {
 pub fn load_bitstream(bitstream: &str) -> Result<(), Box<dyn Error>> {
     let fname = Path::new(bitstream).file_name().ok_or("Failed to extract filename")?
                 .to_str().ok_or("Invalid filename")?;
-
-//  let fname = match path.file_name() {
-//      Some(name) => name.to_str().ok_or("Invalid filename")?,
-//      None => return Err("Failed to extract filename".into()),
-//  };
     let firmware_path = format!("/lib/firmware/{}", fname);
-//  println!("{}", firmware_path);
-//  println!("{}", bitstream);
     let out = command_root("cp", [bitstream, &firmware_path])?;
-//    println!("{}", String::from_utf8_lossy(&out.stdout));
-//    println!("{}", String::from_utf8_lossy(&out.stderr));
-//    println!("{}", out.status);
+    if !out.status.success() {
+        return Err("Failed to copy bitstream".into());
+    }
     let load_cmd = format!("echo {} > /sys/class/fpga_manager/fpga0/firmware", fname);
-    command_root("sh", ["-c", &load_cmd])?;
+    let out = command_root("sh", ["-c", &load_cmd])?;
+    if !out.status.success() {
+        return Err("Failed to load bitstream".into());
+    }
 
     Ok(())
 }
-
